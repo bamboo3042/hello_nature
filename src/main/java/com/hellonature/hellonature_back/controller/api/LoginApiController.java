04666446e962979107c289f84a68be9daf60045e
zngkgk963 <zngkgk963@gmail.com>
2021-10-18 16:38:59 +0900

    멤버 비밀번호 확인 포스트 형식으로 변환

--- src/main/java/com/hellonature/hellonature_back/controller/api/LoginApiController.java
+++ src/main/java/com/hellonature/hellonature_back/controller/api/LoginApiController.java
@@ -2,13 +2,11 @@ package com.hellonature.hellonature_back.controller.api;
 
 import com.hellonature.hellonature_back.model.enumclass.Flag;
 import com.hellonature.hellonature_back.model.network.Header;
+import com.hellonature.hellonature_back.model.network.request.LoginCheckApiRequest;
 import com.hellonature.hellonature_back.model.network.response.LoginApiResponse;
 import com.hellonature.hellonature_back.service.LoginService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/login")
@@ -17,15 +15,20 @@ public class LoginApiController {
 
     private final LoginService loginService;
 
-    @GetMapping("/user")
-    public Header<Flag> userLogin(@RequestParam(name = "id") String id,
-                                  @RequestParam(name = "password") String password){
-        return loginService.userLogin(id, password);
+//    @GetMapping("/user")
+//    public Header<Flag> userLogin(@RequestParam(name = "id") String id,
+//                                  @RequestParam(name = "password") String password){
+//        return loginService.userLogin(id, password);
+//    }
+
+    @PostMapping("/user")
+    public Header<Flag> userLogin(@RequestBody Header<LoginCheckApiRequest> requestHeader){
+        return loginService.userLogin(requestHeader);
     }
 
     @GetMapping("/admin")
     public Header<Flag> adminLogin(@RequestParam(name = "id") String id,
-                                              @RequestParam(name = "password") String password){
+                                   @RequestParam(name = "password") String password){
         return loginService.adminLogin(id, password);
     }
 }
--- src/main/java/com/hellonature/hellonature_back/service/LoginService.java
+++ src/main/java/com/hellonature/hellonature_back/service/LoginService.java
@@ -4,6 +4,7 @@ import com.hellonature.hellonature_back.model.entity.Admin;
 import com.hellonature.hellonature_back.model.entity.Member;
 import com.hellonature.hellonature_back.model.enumclass.Flag;
 import com.hellonature.hellonature_back.model.network.Header;
+import com.hellonature.hellonature_back.model.network.request.LoginCheckApiRequest;
 import com.hellonature.hellonature_back.model.network.response.LoginApiResponse;
 import com.hellonature.hellonature_back.repository.AdminRepository;
 import com.hellonature.hellonature_back.repository.MemberRepository;
@@ -19,7 +20,24 @@ public class LoginService {
     private final AdminRepository adminRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public Header<Flag> userLogin(String id, String password){
+//    public Header<Flag> userLogin(String id, String password){
+//        System.out.println("id : " + id);
+//        System.out.println("password : " + password);
+//        System.out.println("password encode : " + passwordEncoder.encode(password));
+//        return memberRepository.findByEmailAndPassword(id, passwordEncoder.encode(password))
+//                .map(member -> Header.OK(Flag.TRUE))
+//                .orElseGet(() -> Header.ERROR(Flag.FALSE));
+//    }
+
+    public Header<Flag> userLogin(Header<LoginCheckApiRequest> requestHeader){
+
+        LoginCheckApiRequest request = requestHeader.getData();
+
+        String id = request.getId();
+        String password = request.getPassword();
+        System.out.println("id : " + id);
+        System.out.println("password : " + password);
+        System.out.println("password encode : " + passwordEncoder.encode(password));
         return memberRepository.findByEmailAndPassword(id, passwordEncoder.encode(password))
                 .map(member -> Header.OK(Flag.TRUE))
                 .orElseGet(() -> Header.ERROR(Flag.FALSE));
