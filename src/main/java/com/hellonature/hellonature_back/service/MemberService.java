271ff376a10b878ddc45b16370d87d4c29cc2f1f
zngkgk963 <zngkgk963@gmail.com>
2021-10-18 15:32:42 +0900

    마이페이지 정보 수정 - 민지

--- src/main/java/com/hellonature/hellonature_back/service/MemberService.java
+++ src/main/java/com/hellonature/hellonature_back/service/MemberService.java
@@ -72,7 +72,7 @@ public class MemberService extends BaseService<MemberApiRequest, MemberApiRespon
         MemberApiRequest memberApiRequest = request.getData();
         Optional<Member> optional = memberRepository.findById(memberApiRequest.getIdx());
         return optional.map(member -> {
-            member.setPassword(memberApiRequest.getPassword() == null ? null : passwordEncoder.encode(memberApiRequest.getPassword()));
+            if (memberApiRequest.getPassword() != null) member.setPassword(passwordEncoder.encode(memberApiRequest.getPassword()));
             member.setName(memberApiRequest.getName());
             member.setHp(memberApiRequest.getHp());
             member.setGender(memberApiRequest.getGender());
