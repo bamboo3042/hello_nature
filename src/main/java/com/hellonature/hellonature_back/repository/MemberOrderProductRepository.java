30d2bcf616c8c512c5fb52184f1c7cccd036a1b2
0103lys <jkssy1@naver.com>
2021-10-22 10:59:14 +0900

    마이페이지 헬로캐시, 주문내역 정렬(내림차순) 수정

--- src/main/java/com/hellonature/hellonature_back/repository/MemberOrderProductRepository.java
+++ src/main/java/com/hellonature/hellonature_back/repository/MemberOrderProductRepository.java
@@ -2,6 +2,7 @@ package com.hellonature.hellonature_back.repository;
 
 import com.hellonature.hellonature_back.model.entity.MemberOrder;
 import com.hellonature.hellonature_back.model.entity.MemberOrderProduct;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,5 +11,6 @@ import java.util.Optional;
 public interface MemberOrderProductRepository extends JpaRepository<MemberOrderProduct, Long> {
     Optional<MemberOrderProduct> findById(Long id);
     List<MemberOrderProduct> findAllByOrder(MemberOrder order);
-    List<MemberOrderProduct> findAllByOrderIn(List<MemberOrder> orders);
+    List<MemberOrderProduct> findAllByOrderIn(List<MemberOrder> orders, Sort sort);
+
 }
