403ce6a7c8e3d41e3edbb727f0d53162166ac184
dudgns <dudgns334@gmail.com>
2021-10-22 15:05:06 +0900

    상품 리뷰 controller update 수정

--- src/main/java/com/hellonature/hellonature_back/controller/api/ProductReviewApiController.java
+++ src/main/java/com/hellonature/hellonature_back/controller/api/ProductReviewApiController.java
@@ -36,7 +36,7 @@ public class ProductReviewApiController extends CrudController<ProductReviewApiR
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.PUT, consumes = { "multipart/form-data" })
-    public Header<ProductReviewApiResponse> update(@RequestBody ProductReviewApiRequest request,
+    public Header<ProductReviewApiResponse> update(@RequestPart(value = "key") ProductReviewApiRequest request,
                                                    @RequestPart(value = "files") List<MultipartFile> fileList) throws Exception {
         return productReviewService.update(request, fileList);
     }
